f4b2f0aec4933795860970765009809ce07d6cb8
Artem Ponomarev <artponomarev1994@gmail.com>
2022-10-23 18:22:03 +0400

    first commit

---
+++ solution/src/app.js
@@ -0,0 +1,107 @@
+// Copyright 2021 Google LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import './styles.css'
+import { Loader } from '@googlemaps/js-api-loader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+
+
+
+const apiOptions = {
+  apiKey: 'AIzaSyAdNE4Sro-yiLGL4zEagrENhG6JQBvzImc',
+  version: "beta"
+};
+
+const mapOptions = {
+  "tilt": 40,
+  "heading": 30,
+  "zoom": 20,
+  "center": { lat: 41.406704711892924, lng: 2.1615649441710687 }, 
+  "mapId": "75b6f80f67b93337"    
+}
+
+async function initMap() {    
+  const mapDiv = document.getElementById("map");
+  const apiLoader = new Loader(apiOptions);
+  await apiLoader.load();
+  return new google.maps.Map(mapDiv, mapOptions);
+}
+
+
+function initWebGLOverlayView(map) {  
+  let scene, renderer, camera, loader;
+  const webGLOverlayView = new google.maps.WebGLOverlayView();
+  
+  webGLOverlayView.onAdd = () => {   
+    // set up the scene
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(); // cam. param.( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+    const ambientLight = new THREE.AmbientLight( 0xffffff, 0.75 ); // soft white light
+    scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.25);
+    directionalLight.position.set(0.5, -1, 0.5);
+    scene.add(directionalLight);
+  
+    // load the model    
+    loader = new GLTFLoader();               
+    const source = "./assets/models/uploads_files_3688322_one_living_room.glb";
+    loader.load(
+      source,
+      gltf => {      
+        gltf.scene.scale.set(0.1,0.1,0.1);
+        gltf.scene.rotation.x = 90 * Math.PI/180; // rotations are in radians
+        gltf.scene.rotation.y = 126 * Math.PI/180; // rotations are in radians
+        scene.add(gltf.scene);           
+      }
+    );
+  }
+  
+  webGLOverlayView.onContextRestored = ({gl}) => {    
+    // create the three.js renderer, using the
+    // maps's WebGL rendering context.
+    renderer = new THREE.WebGLRenderer({
+      canvas: gl.canvas,
+      context: gl,
+      ...gl.getContextAttributes(),
+    });
+    renderer.autoClear = false;
+
+    // wait to move the camera until the 3D model loads    
+    
+  }
+
+  webGLOverlayView.onDraw = ({gl, transformer}) => {
+    // update camera matrix to ensure the model is georeferenced correctly on the map
+    const latLngAltitudeLiteral = {
+        lat: mapOptions.center.lat,
+        lng: mapOptions.center.lng,
+        altitude: 20
+    }
+
+    const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
+    camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
+    
+    webGLOverlayView.requestRedraw();      
+    renderer.render(scene, camera);                  
+
+    // always reset the GL state
+    renderer.resetState();
+  }
+  webGLOverlayView.setMap(map);
+}
+
+(async () => {        
+  const map = await initMap();
+  initWebGLOverlayView(map);    
+})();
---
+++ solution/webpack.config.js
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2021 Google LLC
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *  https://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
+
+const CONFIG = {
+  mode,
+  target,
+  devtool,
+
+  entry: {
+    app: ["@babel/polyfill", path.resolve(__dirname, 'src', 'app.js')]
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    }
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[name][ext]'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: [  devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      // {
+      //   test: /\.m?js$/,
+      //   exclude: /(node_modules|bower_components)/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env']
+      //     }
+      //   }
+      // },
+      {
+        test: /\.(glb|gltf|obj|png|jpg|fbx|dae|css)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/models/',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              },
+            }
+          }
+        ],
+        //type: 'asset/resource',
+      }
+    ]
+  }
+
+};
+
+// This line enables bundling against src in this repo rather than installed module
+module.exports = CONFIG;
